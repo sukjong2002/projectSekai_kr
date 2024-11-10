fda997d4dd4d864cb7c8eff3b6cf2ce0d95b5330
sukjong2002 <sukjong2002@gmail.com>
2024-11-11 02:02:05 +1100

    initial changes

--- README.md
+++ README.md
@@ -1,20 +1,10 @@
-# projectSekai
-
-## Update
-
-开新坑了 暂时无限搁置各种bot插件开发 
-Client已经粗糙的实现了 想必看到这个仓库的你也会着手开发对应的功能了吧
+# projectSekai_kr
 
 ## Description
 
-这是一个关于游戏[プロセカ](https://pjsekai.sega.jp/)(世界计划 多彩舞台)的相关功能插件
+This program is a implementation for [프로세카](https://www.kr-pjsekai.com/), which is the Korean Version of [プロセカ](https://pjsekai.sega.jp/) game.<br>
+The original source for this program can be accessed via the following link: https://github.com/xhl6666/projectSekai
 
-本插件适用于[HoshinoBot v2](https://github.com/Ice-Cirno/HoshinoBot)
-
-项目地址: https://github.com/xhl6666/projectSekai
-
-请注意: 插件所提供功能仅支持由[ColorFulPalette](https://colorfulpalette.co.jp/)运营的日服版本, 暂不支持繁中服以及国际服
-
-本项目与SEGA, ColorFul Palette等公司无任何关系, 注册商标所有权归相关品牌所有
-
-本项目使用GPL-3.0协议, 使用本插件所造成的一切财产损失或其他形式损失, 均与开发者无关, 开发者不承担任何相关责任
+The code is subject to the terms of the GPL3.0 licence, which is the same licence that applies to the original code.<br>
+This program is not affiliated with SEGA, ColorFul Palette, etc. The registered trademarks are the property of the respective brands.<br>
+The use of this plug-in caused by all property damage or other forms of loss, are not related to the developer, the developer does not assume any responsibility for the relevant
--- client.py
+++ client.py
@@ -5,38 +5,39 @@ import uuid
 import requests
 import base64
 import json
+import urllib
 
 class SekaiUser(object):
     
-    def __init__(self, uid, credit):
-        self.uid = uid
-        self.credential = credit
+    def __init__(self, lid, iid, deviceId):
+        self.lid = lid
+        self.iid = iid
+        self.deviceId = deviceId
         
 
 class SekaiClient(object):
     
     def __init__(self, user:SekaiUser=None):
-        self.urlroot = "http://production-game-api.sekai.colorfulpalette.org/api"
-        self.urlroot2 = "https://production-game-api.sekai.colorfulpalette.org/api"
+        self.urlroot = "https://mkkorea-obt-prod01-cdn.bytedgame.com/api"
+        self.urlroot2 = "https://mkkorea-obt-prod02.bytedgame.com/api"
         self.headers = {
             "Content-Type": "application/octet-stream",
             "Accept": "application/octet-stream",
             "Accept-Encoding": "deflate, gzip",
-            "Host": "production-game-api.sekai.colorfulpalette.org",
-            "User-Agent": "UnityPlayer/2019.4.3f1 (UnityWebRequest/1.0, libcurl/7.52.0-DEV)",
-            "X-Install-Id": "baf5217d-a4d6-42b3-8c28-c4652337721b",
-            "X-App-Version": "1.9.0",
-            "X-Asset-Version": "1.9.0.10",
-            "X-Data-Version": "1.9.0.10",
+            "Host": "mkkorea-obt-prod01-cdn.bytedgame.com",
+            "User-Agent": "UnityPlayer/2020.3.32f1 (UnityWebRequest/1.0, libcurl/7.80.0-DEV)",
+            "X-Install-Id": "8762943b-39c0-4716-9d9d-89b4b3382415",
+            "X-App-Version": "3.4.0.15133",
+            "X-App-Hash": "a3015fe8-785f-27e1-fb8b-546a23c82c1f",
+            "X-Data-Version": "3.4.0.13",
+            "X-Asset-Version": "3.4.0",
             "X-Platform": "Android",
-            "X-GA": "e81d8234-b443-4193-9891-0a736c4b1546",
-            "X-DeviceModel": "Xiaomi M114514C",
-            "X-OperatingSystem": "Android OS 10 / API-29 (HUAWEISEA-AL10/10.1.0.164C00)",
-            "X-MA": "F8:9A:78:5F:C8:04",
-            "X-Unity-Version": "2019.4.3f1",
-            "X-AI": "20f48346fad7f921245a8db7fdfb734f"
-        }
-        self.headers["X-Request-Id"] = str(uuid.uuid1())
+            "device_id": user.deviceId,
+            "X-DeviceModel": "Samsung SM-X710",
+            "X-OperatingSystem": "Android OS 12 / API-32 (SP2A.220505.003/14.0.609_230420)",
+            "X-Unity-Version": "2020.3.32f1"
+            }
+        self.headers["X-Request-Id"] = user.lid
         self.user = user
         
     @staticmethod
@@ -59,47 +60,114 @@ class SekaiClient(object):
         plaintext = cryptor.decrypt(encrypted)
         return msgpack.unpackb(plaintext[:-plaintext[-1]],strict_map_key = False)
 
-    def callapi(self, apiurl, method, content=None):
+    def callapi(self, apiurl, method, content=None, root=None, hd=None):
         content = None if not content else self.pack(content)
-        resp = requests.request(method, url=self.urlroot2+apiurl, headers=self.headers, data=content, timeout=5)
+        print(self.urlroot2+apiurl if root==None else root+apiurl)
+        resp = requests.request(method, url=self.urlroot2+apiurl if root==None else root+apiurl, headers=self.headers if hd==None else hd, data=content, timeout=5)
+        # print(resp.content)
         return self.unpack(resp.content)
 
-    def calluserapi(self, uid, apiurl, method, content=None):
-        return self.callapi(f"/user/{uid}{apiurl}", method, content)
+    def calluserapi(self, apiurl, method, content=None):
+        return self.callapi(f"/user{apiurl}", method, content, root=self.urlroot)
     
-    def register(self) -> SekaiUser:
-        data = self.callapi("/user", "POST", {'platform': self.headers["X-Platform"], 'deviceModel': self.headers["X-DeviceModel"], 'operatingSystem': self.headers["X-OperatingSystem"]})
-        credential = data["credential"]
-        uid = data["userRegistration"]["userId"]
-        self.user = SekaiUser(uid, credential)
-        return self.user
+    # def register(self) -> SekaiUser:
+    #     data = self.callapi("/user", "POST", {'platform': self.headers["X-Platform"], 'deviceModel': self.headers["X-DeviceModel"], 'operatingSystem': self.headers["X-OperatingSystem"]})
+    #     credential = data["credential"]
+    #     uid = data["userRegistration"]["userId"]
+    #     self.user = SekaiUser(uid, credential)
+    #     return self.user
+
+    def getAccessToken(self, lid):
+        url_visitor = "https://gsdk-sg.bytegsdk.com/sdk/account/"
+        url_login = "https://gsdk-sg.bytegsdk.com/gsdk/account/"
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'User-Agent': 'com.pjsekai.kr.oscb/15133 (Linux; U; Android 12; ko_KR; SM-X710; Build/SP2A.220505.003; Cronet/TTNetVersion:c10fb468 2023-04-11 QuicVersion:b314d107 2021-11-24)',
+        }
+        params = {
+            "iid": user.iid,
+            "device_id": user.deviceId,
+            "ac": "wifi",
+            "channel": "onestore",
+            "aid": "292248",
+            "app_name": "mk_korea",
+            "device_platform": "android",
+            "os": "android",
+            "ssmix": "a",
+            "device_type": "SM-X710",
+            "device_brand": "Samsung",
+            "language": "ko",
+            "os_api": "32",
+            "os_version": "12",
+            "manifest_version_code": "15133",
+            "sdk_language": "ko_KR",
+            "login_way": "guest",
+            "sys_region": "KR",
+            "sdk_app_id": "1782",
+            "tz_offset": "36000",
+            "channel_op": "bsdkintl",
+            "sdk_version": "2024.1.0.0",
+            "game_id": "292248"
+        }
+        data_visitor = {
+            "login_id": lid,
+            "user_type": "1",
+            "ui_flag": "1",
+            "is_create": "0"
+        }
+        data_login = {
+            "login_id": lid,
+            "adjust_id": "",
+            "device_id": user.deviceId,
+            "iid":	user.iid,
+            "login_type":	"history",
+            "ui_flag":	"1",
+            "channel_id":	"bsdkintl"
+        }
+        resp = requests.post(url=url_visitor+'visitor_login', params=params, data=data_visitor)
+        res = json.loads(resp.content)
+        print(res)
+        data_login['data'] = json.dumps({'user_id':str(res['data']['user_id']),'token':str(res['data']['token'])})
+        # print(data_login)
+        resp = requests.post(url=url_login+'login', params=params, data=data_login)
+        # print(resp.request.body)
+        print(resp.content)
+        res = json.loads(resp.content)
+        return res['data']['access_token']
         
     def login(self):
-        data = self.calluserapi(self.user.uid, "/auth?refreshUpdatedResources=False", "PUT", {"credential": self.user.credential})
+        accessToken = self.getAccessToken(user.lid)
+        data = self.calluserapi("/auth", "POST", {"accessToken": accessToken, "deviceId": None, "UserID": 0})
+        # print(data)
         self.token = data["sessionToken"]
         self.headers["X-Session-Token"] = self.token
-        self.update_headers()
         return self.token
     
-    def update_headers(self):
-        data = self.callapi("/system", "GET")
-        for v in data["appVersions"]:
-            if not v["appVersionStatus"] == "available": continue
-            self.headers["X-Data-Version"] = v["dataVersion"]
-            self.headers["X-Asset-Version"] = v["assetVersion"]
-            self.headers["X-App-Version"] = v["appVersion"]
-    
     def get_profile(self, uid:str):
-        data = self.calluserapi(uid, "/profile", "GET")
+        data = self.calluserapi("/{}/profile".format(uid), "GET")
         return data
         
     def test(self):
         pass
 
-    
-# user = SekaiUser("129502785700777984", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjcmVkZW50aWFsIjoiZmZiZGZmNGMtZDQ2My00ZjIxLThlMzMtOGQ5NDE2NzY4MjA0IiwidXNlcklkIjoiMTI5NTAyNzg1NzAwNzc3OTg0In0.Z-jZk0kd-8YxuKjXxvGFJAWlgDzmSh0NElgqqOlf_5Q")
-# sekai = SekaiClient(user)
-# # sekai.register()
-# sekai.login()
-# a = sekai.get_profile("5557466289360906")
+with open('config.json', 'r') as f:
+    config = json.load(f)
 
+uuId = config['uuId']
+loginId = config['loginId']
+iid = config['iid']
+
+friendId = ""
+
+user = SekaiUser(uuId, loginId, iid)
+sekai = SekaiClient(user)
+#  sekai.register()
+sekai.login()
+a = sekai.get_profile(friendId)
+print(a)
+# # print(a)
+
+# output = base64.b64decode(b64.encode())
+# result = sekai.unpack(output)
+# with open('output.json', 'w', encoding='utf-8') as f:
+#     json.dump(result, f, ensure_ascii=False, indent=4)
